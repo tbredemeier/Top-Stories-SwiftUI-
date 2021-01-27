f9e227f2959559d377d1c78992c1b3dcc0facf6f
Tom Bredemeier <tbredemeier@barrington220.org>
2021-01-27 14:10:30 -0600

    Initial Commit

---
+++ Top Stories (SwiftUI)/Top_Stories__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Top_Stories__SwiftUI_App.swift
+//  Top Stories (SwiftUI)
+//
+//  Created by Tom Bredemeier on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Top_Stories__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
